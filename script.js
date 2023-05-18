2baebef50079e1377ff6c2680b58915899f40b6b
JL <dreamsend08@gmail.com>
2023-05-18 18:39:05 +0800

    Add a function that starts a round

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 function getComputerChoice(){
     const choices = ["Rock", "Scissors", "Papers"];
-    return Math.floor(Math.random()* 3) + 1;
+    return choices[Math.floor(Math.random()* 3)];
+} 
+
+function startRound(player, computer){
+    let playerChoice = player.toLowerCase();
+    let computerChoice = computer.toLowerCase();
+    if (
+        (playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "scissors" && computerChoice === "paper") ||
+        (playerChoice === "paper" && computerChoice === "rock")
+    ) {
+        return "You win! ${playerChoice} beats ${computerChoice}";
+    } else if (computerChoice === playerChoice){
+        return "Draw"
+    } else {
+        return "You lose"
+    }
 }
