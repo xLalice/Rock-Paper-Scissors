3d25de4a862631fcef3205a93276edac6a69abe3
JL <dreamsend08@gmail.com>
2023-05-18 19:42:04 +0800

    Add main function to play game

--- script.js
+++ script.js
@@ -4,17 +4,42 @@ function getComputerChoice(){
 } 
 
 function startRound(player, computer){
-    let playerChoice = player.toLowerCase();
-    let computerChoice = computer.toLowerCase();
+    let playerSelection = player.toLowerCase();
+    let computerSelection= computer.toLowerCase();
     if (
-        (playerChoice === "rock" && computerChoice === "scissors") ||
-        (playerChoice === "scissors" && computerChoice === "paper") ||
-        (playerChoice === "paper" && computerChoice === "rock")
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "rock")
     ) {
-        return "You win! ${playerChoice} beats ${computerChoice}";
-    } else if (computerChoice === playerChoice){
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else if (computerSelection === playerSelection){
         return "Draw"
     } else {
         return "You lose"
     }
 }
+
+
+
+function game(){
+    //Record the scores of the player and the computer
+    let playerScore = 0;
+    let computerScore = 0;
+
+    //If either one reach 5, the game is over, until then keep playing
+    while (playerScore != 5 && computerScore != 5) {
+        let playerChoice = prompt("Enter your weapon");
+        let computerChoice = getComputerChoice();
+        console.log(playerChoice);
+        let result = startRound(playerChoice, computerChoice);
+        console.log(result);
+        if (result.includes("win")) {
+            playerScore += 1;
+        } else if (result.includes("lose")) {
+            computerScore+= 1;
+        }
+        console.log(`You: ${playerScore}   Computer: ${computerScore}`)
+    }
+}
+
+game();
