5d358f8f22b9c9566420f81209201fd8a019acdf
xLalice <dreamsend08@gmail.com>
2023-06-26 17:37:32 +0800

    Refactor code

--- script.js
+++ script.js
@@ -6,59 +6,67 @@ const comScoreCont = document.getElementById("computer-score");
 const result = document.getElementById("result");
 const buttons = document.querySelectorAll("button");
 
-
-function getComputerChoice(){
-    const choices = ["Rock", "Scissors", "Paper"];
-    return choices[Math.floor(Math.random()* 3)];
-} 
-
-
-
-function startRound(playerSelection){
-    let computerSelection= getComputerChoice();
-    const computerChoice = document.getElementById(computerSelection);
-
-    
-    if (
-        (playerSelection === "Rock" && computerSelection === "Scissors") ||
-        (playerSelection === "Scissors" && computerSelection === "Paper") ||
-        (playerSelection === "Paper" && computerSelection === "Rock")
-    ) {
-        result.textContent =  `You win! ${playerSelection} beats ${computerSelection}`;
-        playerScore += 1;
-        playerScoreCont.textContent = playerScore;
-    } else if (computerSelection === playerSelection){
-        result.textContent = "Draw";
-    } else {
-        result.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
-        computerScore += 1;
-        comScoreCont.textContent = computerScore;
-    }
-
-    if (playerScore >= bestOf || computerScore >= bestOf){
-        let winner = playerScore > computerScore ? "Win" : "Lose";
-        result.textContent = `Game Over! You ${winner}`;
-        playerScore = 0;
-        computerScore = 0;
-        playerScoreCont.textContent = playerScore;
-        comScoreCont.textContent = computerScore;
-        
-        return;
-    }
+function getComputerChoice() {
+  const choices = ["Rock", "Scissors", "Paper"];
+  return choices[Math.floor(Math.random() * 3)];
 }
 
+function animateElement(element) {
+  element.style.transform = "scale(1.1)";
+  setTimeout(() => {
+    element.style.transform = "scale(1)";
+  }, 500);
+}
 
-    
+function updateScores(playerSelection, computerSelection) {
+  if (
+    (playerSelection === "Rock" && computerSelection === "Scissors") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper") ||
+    (playerSelection === "Paper" && computerSelection === "Rock")
+  ) {
+    result.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+    playerScore += 1;
+    playerScoreCont.textContent = playerScore;
+  } else if (computerSelection === playerSelection) {
+    result.textContent = "Draw";
+  } else {
+    result.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+    computerScore += 1;
+    comScoreCont.textContent = computerScore;
+  }
+}
 
-// Event listener setup
+function checkGameOver() {
+  if (playerScore >= bestOf || computerScore >= bestOf) {
+    let winner = playerScore > computerScore ? "Win" : "Lose";
+    result.textContent = `Game Over! You ${winner}`;
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreCont.textContent = playerScore;
+    comScoreCont.textContent = computerScore;
+    return true;
+  }
+  return false;
+}
+
+function startRound(playerSelection) {
+  let computerSelection = getComputerChoice();
+  const computerChoice = document.getElementById(computerSelection);
+  animateElement(computerChoice);
+  updateScores(playerSelection, computerSelection);
+
+  if (checkGameOver()) {
+    return;
+  }
+}
+
 function setupEventListeners() {
-    buttons.forEach(button => {
-        button.addEventListener("click", () => {
-            startRound(button.id);
-        });
+  buttons.forEach(button => {
+    button.addEventListener("click", () => {
+      startRound(button.id);
     });
+  });
 }
 
 // Call the function to set up event listeners
 setupEventListeners();
-
